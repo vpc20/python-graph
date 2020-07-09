0fd9dea07f1c72620e518890e825a87c7afa97a7
vpc <cuevasvp@gmail.com>
2020-07-09 16:52:32 +0800

    initial commit

--- ConnectedComponents.py
+++ ConnectedComponents.py
@@ -1,194 +1,147 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 
 from DisjointSet import DisjointSet
-from Graph import transpose, Graph
+from Graph import Graph, Digraph
+from Transpose import transpose
+
+
+def is_connected(g):
+    """
+    An undirected graph is connected if every vertex is reachable from all other vertices.
+
+    :param g: input Graph
+    :return: True if the graph is connected, False otherwise.
+    """
+
+    def dfs(g, u):
+        seen.add(u)
+        for v in g.neighbors(u):
+            if v not in seen:
+                dfs(g, v)
+
+    count = 0
+    seen = set()
+    for u in g.vertices:
+        if u not in seen:
+            count += 1  # count connected components
+            if count > 1:
+                return False
+            dfs(g, u)
+    return True
 
 
 def connected_components(g):
     """
-    The connected components of a graph are the equivalence classes of vertices under 
+    The connected components of a graph are the equivalence classes of vertices under
     the “is reachable from” relation.
-     
-    :param g: input Graph 
-    :return: list of sets containg the connected vertices
+
+    :param g: input Graph
+    :return: list of sets contaning the connected vertices
     """
-    djset = DisjointSet()
-    for v in g.vertices():
-        djset.make_set(v)
+    def dfs(g, u):
+        seen.add(u)
+        conns[-1].append(u)
+        for v in g.neighbors(u):
+            if v not in seen:
+                dfs(g, v)
 
-    for v1, v2 in g.edges():
-        if djset.find_set(v1) != djset.find_set(v2):
-            djset.union(v1, v2)
+    conns = []
+    seen = set()
+    for u in g.vertices:
+        if u not in seen:
+            conns.append([])
+            dfs(g, u)
+    return conns
 
-    return djset.get_set()
 
-
-# def connected_components_naive(graph):
+# def connected_components_dj(g):
 #     djset = DisjointSet()
+#     for v in g.vertices:
+#         djset.make_set(v)
 #
-#     for vertex in graph.vertices():
-#         djset.make_set(vertex)
+#     for v1, v2 in g.edges:
+#         if djset.find_set(v1) != djset.find_set(v2):
+#             djset.union(v1, v2)
 #
-#     for edge1, edge2 in graph.edges():
-#         set1 = djset.find_set(edge1)
-#         set2 = djset.find_set(edge2)
-#         if set1 != set2:
-#             djset.union(edge1, edge2)
-#     return djset.disj_list
-
-
-# def connected_components(graph):
-#     djset = DisjointSet()
-#     vertex_seq = defaultdict(str)
-#     for i, vertex in enumerate(graph.vertices()):
-#         vertex_seq[vertex] = i
-#         djset.make_set(i)
-#
-#     for edge1, edge2 in graph.edges():
-#         par1 = djset.find_set(vertex_seq[edge1])
-#         par2 = djset.find_set(vertex_seq[edge2])
-#         if par1 != par2:
-#             djset.union(vertex_seq[edge1], vertex_seq[edge2])
-#
-#     parent_ids = djset.get()
-#     # print(parent_ids)
-#     conn_comps = defaultdict(set)
-#     for i, vertex in enumerate(graph.vertices()):
-#         conn_comps[parent_ids[i]].add(vertex)
-#     return list(conn_comps.values())
-
-
-def connected_components_dfs(graph):
-    vlist = [v for v in graph.vertices()]
-    _, ftime, _, _ = strongly_connected_dfs(graph, vlist)
-
-    ftime_list = [(k, v) for k, v in ftime.items()]
-    ftime_list.sort(key=lambda e: e[1], reverse=True)
-    vlist = [v for v, _ in ftime_list]
-
-    _, _, _, vert_dfs_tree = strongly_connected_dfs(graph, vlist)
-    [set(item) for item in vert_dfs_tree.values()]
-    return [set(item) for item in vert_dfs_tree.values()]
-
-
-# def connected_components_dfs(graph):
-#     def _connected_components(vertex):
-#         for neighbor in graph.adj_list[vertex]:
-#             if neighbor not in visited:
-#                 visited.add(neighbor)
-#                 conn_components[cc_id].add(neighbor)
-#                 _connected_components(neighbor)
-#
-#     conn_components = defaultdict(set)
-#     visited = set()
-#     cc_id = 0
-#
-#     for vertex in graph.vertices():
-#         if vertex not in visited:
-#             visited.add(vertex)
-#             cc_id += 1
-#             conn_components[cc_id].add(vertex)
-#             _connected_components(vertex)
-#     return list(conn_components.values())
-
-# def dfs_connection_dict(graph):
-#     connected_dict = defaultdict(int)
-#     visited = set()
-#     cc_id = 0
-#     for vertex in graph.vertices():
-#         if vertex not in visited:
-#             cc_id += 1
-#             visited.add(vertex)
-#             connected_dict[vertex] = cc_id
-#             _dfs_connection_dict(graph, vertex, visited, cc_id, connected_dict)
-#     return connected_dict
-#
-#
-# def _dfs_connection_dict(graph, vertex, visited, cc_id, connected_dict):
-#     for neighbor in graph.adj_list[vertex]:
-#         if neighbor not in visited:
-#             visited.add(neighbor)
-#             connected_dict[neighbor] = cc_id
-#             _dfs_connection_dict(graph, neighbor, visited, cc_id, connected_dict)
+#     return djset.get_set()
 
 
 def strongly_connected_components(g):
     """
-    The strongly connected components of a directed graph are the equivalence classes of 
+    The strongly connected components of a directed graph are the equivalence classes of
     vertices under the “are mutually reachable” relation.
-     
+
     :param g: input graph
     :return: list of sets containing the strongly connected vertices
     """
-    vert_list = [vertex for vertex in g.vertices()]
-    _, ftime, _, _ = strongly_connected_dfs(g, vert_list)
+
+    def dfs(g, u):
+        seen.add(u)
+        for v in g.neighbors(u):
+            if v not in seen:
+                dfs(g, v)
+        verts.appendleft(u)
+
+    def dfs_gt(gt, u):
+        seen.add(u)
+        conns[-1].append(u)
+        for v in gt.neighbors(u):
+            if v not in seen:
+                dfs_gt(gt, v)
+
+    seen = set()
+    verts = deque()  # vertices in order of decreasing finishing time
+    for u in g.vertices:
+        if u not in seen:
+            dfs(g, u)
 
     gt = transpose(g)
-
-    ftime_list = [(k, v) for k, v in ftime.items()]
-    ftime_list.sort(key=lambda e: e[1], reverse=True)
-    vert_list = [v for v, _ in ftime_list]
-    _, _, _, vert_dfs_tree = strongly_connected_dfs(gt, vert_list)
-    return [set(val) for val in vert_dfs_tree.values()]
-
-
-def strongly_connected_dfs(graph, vert_list):
-    def _dfs(graph, vertex):
-        nonlocal visited, time
-        time += 1
-        dtime[vertex] = time
-        for neighbor in graph.neighbors(vertex):
-            if neighbor not in visited:
-                vert_dfs_tree[cc_id].append(neighbor)
-                pred[neighbor] = vertex
-                visited.add(neighbor)
-                _dfs(graph, neighbor)
-        time += 1
-        ftime[vertex] = time
-
-    visited = set()
-    time = 0
-    dtime = defaultdict(int)  # discovery time
-    ftime = defaultdict(int)  # finishing time
-    pred = defaultdict()  # predecessors of vertex
-    cc_id = 0
-    vert_dfs_tree = defaultdict(list)  # vertices of each tree in the depth-first forest
-    for vertex in graph.vertices():  # initialize
-        pred[vertex] = None
-
-    # for vertex in graph.vertices():
-    for vertex in vert_list:
-        if vertex not in visited:
-            cc_id += 1
-            vert_dfs_tree[cc_id].append(vertex)
-            visited.add(vertex)
-            _dfs(graph, vertex)
-    return dtime, ftime, pred, vert_dfs_tree
+    seen = set()
+    conns = []
+    for u in verts:
+        if u not in seen:
+            conns.append([])
+            dfs_gt(gt, u)
+    return conns
 
 
 if __name__ == '__main__':
     g = Graph()
-    g.add_edge('a', 'b')
-    g.add_edge('a', 'c')
-    g.add_edge('b', 'c')
-    g.add_edge('b', 'd')
-    g.add_edge('e', 'f')
-    g.add_edge('e', 'g')
-    g.add_edge('h', 'i')
+    g.add_edges_from([('a', 'b'), ('a', 'c'), ('b', 'c'), ('b', 'd'),
+                      ('e', 'f'), ('e', 'g'), ('h', 'i')])
     g.add_vertex('j')
+    print(f'connected: {is_connected(g)}')
+    # print(connected_components_dj(g))
     print(connected_components(g))
-    print(connected_components_dfs(g))
+    assert connected_components(g) == [['a', 'b', 'c', 'd'], ['e', 'f', 'g'], ['h', 'i'], ['j']]
+
+    g = Graph()  # clrs Figure B.2(b)
+    g.add_edges_from([(1, 2), (1, 5), (2, 5), (3, 6)])
+    g.add_vertex(4)
+    print(f'connected: {is_connected(g)}')
+    # print(connected_components_dj(g))
+    print(connected_components(g))
+    assert connected_components(g) == [[1, 2, 5], [3, 6], [4]]
 
     g = Graph()
-    g.add_edge(0, 1)
-    g.add_edge(1, 2)
-    g.add_edge(1, 3)
-    g.add_edge(2, 3)
-    g.add_edge(3, 4)
-    g.add_vertex(5)
-    g.add_edge(6, 7)
-    g.add_edge(6, 8)
-    # [{6, 7, 8}, {5}, {0, 1, 2, 3, 4}]
-    print(strongly_connected_components(g))
+    g.add_edges_from([(1, 2), (1, 5), (2, 3), (2, 5), (3, 6), (4, 5)])
+    print(f'connected: {is_connected(g)}')
+    # print(connected_components_dj(g))
+    print(connected_components(g))
+    assert connected_components(g) == [[1, 2, 3, 6, 5, 4]]
 
+    dg = Digraph()
+    dg.add_edges_from([('a', 'b'), ('b', 'c'), ('b', 'e'), ('c', 'd'), ('c', 'g'), ('d', 'c'), ('d', 'h'),
+                       ('e', 'a'), ('e', 'f'), ('f', 'g'), ('g', 'f'), ('g', 'h'), ('h', 'h'), ])
+    print(strongly_connected_components(dg))
+    assert strongly_connected_components(dg) == [['a', 'e', 'b'], ['c', 'd'], ['g', 'f'], ['h']]
 
+    dg = Digraph()
+    dg.add_edges_from([(0, 1), (1, 3), (2, 1), (3, 2), (3, 4), (4, 5), (5, 7), (6, 4), (7, 6)])
+    print(strongly_connected_components(dg))
+    assert strongly_connected_components(dg) == [[0], [1, 2, 3], [4, 6, 7, 5]]
+
+    dg = Digraph()
+    dg.add_edges_from([(0, 1), (0, 2), (0, 3), (1, 3), (1, 4), (2, 4), (3, 4)])
+    print(strongly_connected_components(dg))
+    assert strongly_connected_components(dg) == [[0], [2], [1], [3], [4]]
--- test_connected_components.py
+++ test_connected_components.py
@@ -1,114 +1,118 @@
+from random import randint
 from unittest import TestCase
 
-from ConnectedComponents import connected_components, connected_components_dfs, strongly_connected_components
-from Graph import Graph, Digraph
+from ConnectedComponents import connected_components, strongly_connected_components
+import networkx as nx
+import Graph as gx
 
 
 class TestConnectedComponents(TestCase):
     def test_connected_components(self):
-        g = Graph()
-        g.add_edge('a', 'b')
-        g.add_edge('a', 'c')
-        g.add_edge('b', 'c')
-        g.add_edge('b', 'd')
-        g.add_edge('e', 'f')
-        g.add_edge('e', 'g')
-        g.add_edge('h', 'i')
-        g.add_vertex('j')
-        print(connected_components(g))
-        print(connected_components_dfs(g))
-        self.assertEqual(sorted([sorted(list(item)) for item in (connected_components(g))]),
-                         sorted([sorted(list(item)) for item in (connected_components_dfs(g))]))
+        for _ in range(1000):
+            nxg = nx.fast_gnp_random_graph(randint(1, 10), randint(1, 9) / 10)
+            print(sorted(nxg.nodes))
+            print(sorted(nxg.edges))
 
-    def test_strongly_connected_components(self):
-        # cp3 4.1 in https://visualgo.net/en/dfsbfs
-        g = Graph()
-        g.add_edge(0, 1)
-        g.add_edge(1, 2)
-        g.add_edge(1, 3)
-        g.add_edge(2, 3)
-        g.add_edge(3, 4)
-        g.add_vertex(5)
-        g.add_edge(6, 7)
-        g.add_edge(6, 8)
-        self.assertEqual([{6, 7, 8}, {5}, {0, 1, 2, 3, 4}], strongly_connected_components(g))
+            g = gx.Graph()
+            for node in nxg.nodes:
+                g.add_vertex(node)
+            for u, v in nxg.edges:
+                g.add_edge(u, v)
+            print(sorted(g.vertices))
+            print(sorted(g.edges))
 
-        # cp3 4.3 in https://visualgo.net/en/dfsbfs
-        g = Graph()
-        g.add_edge(0, 1)
-        g.add_edge(0, 4)
-        g.add_edge(1, 2)
-        g.add_edge(1, 5)
-        g.add_edge(2, 3)
-        g.add_edge(2, 6)
-        g.add_edge(3, 7)
-        g.add_edge(4, 8)
-        g.add_edge(5, 6)
-        g.add_edge(5, 10)
-        g.add_edge(6, 11)
-        g.add_edge(7, 12)
-        g.add_edge(8, 9)
-        g.add_edge(9, 10)
-        g.add_edge(10, 11)
-        g.add_edge(11, 12)
-        # print(strongly_connected_components(g))
-        self.assertEqual([{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}], strongly_connected_components(g))
+            self.assertEqual(sorted([sorted(list(e)) for e in nx.connected_components(nxg)]),
+                             sorted([sorted(e) for e in connected_components(g)]))
 
-        # cp3 4.4 dag in https://visualgo.net/en/dfsbfs
-        dg = Digraph()
-        dg.add_edge(0, 1)
-        dg.add_edge(0, 2)
-        dg.add_edge(1, 2)
-        dg.add_edge(1, 3)
-        dg.add_edge(2, 3)
-        dg.add_edge(2, 5)
-        dg.add_edge(3, 4)
-        dg.add_edge(7, 6)
-        # print(strongly_connected_components(dg))
-        self.assertEqual([{7}, {6}, {0}, {1}, {2}, {5}, {3}, {4}], strongly_connected_components(dg))
-
-        # cp3 4.9 in https://visualgo.net/en/dfsbfs
-        dg = Digraph()
-        dg.add_edge(0, 1)
-        dg.add_edge(1, 3)
-        dg.add_edge(2, 1)
-        dg.add_edge(3, 2)
-        dg.add_edge(3, 4)
-        dg.add_edge(4, 5)
-        dg.add_edge(5, 7)
-        dg.add_edge(6, 4)
-        dg.add_edge(7, 6)
-        # print(strongly_connected_components(dg))
-        self.assertEqual([{0}, {1, 2, 3}, {4, 5, 6, 7}], strongly_connected_components(dg))
-
-        # cp3 4.17 dag in https://visualgo.net/en/dfsbfs
-        dg = Digraph()
-        dg.add_edge(0, 1)
-        dg.add_edge(0, 2)
-        dg.add_edge(0, 3)
-        dg.add_edge(1, 3)
-        dg.add_edge(1, 4)
-        dg.add_edge(2, 4)
-        dg.add_edge(3, 4)
-        # print(strongly_connected_components(dg))
-        self.assertEqual([{0}, {2}, {1}, {3}, {4}], strongly_connected_components(dg))
-
-        # cp3 4.18 dag bipartite in https://visualgo.net/en/dfsbfs
-        dg = Digraph()
-        dg.add_edge(0, 1)
-        dg.add_edge(0, 2)
-        dg.add_edge(1, 3)
-        dg.add_edge(2, 3)
-        dg.add_edge(3, 4)
-        print(strongly_connected_components(dg))
-        self.assertEqual([{0}, {2}, {1}, {3}, {4}], strongly_connected_components(dg))
-
-        # cp3 4.19 bipartite in https://visualgo.net/en/dfsbfs
-        dg = Digraph()
-        dg.add_edge(0, 1)
-        dg.add_edge(0, 4)
-        dg.add_edge(1, 2)
-        dg.add_edge(2, 1)
-        dg.add_edge(2, 3)
-        print(strongly_connected_components(dg))
-        self.assertEqual([{0}, {4}, {1, 2}, {3}], strongly_connected_components(dg))
+            # def test_strongly_connected_components(self):
+            #     # cp3 4.1 in https://visualgo.net/en/dfsbfs
+            #     g = Graph()
+            #     g.add_edge(0, 1)
+            #     g.add_edge(1, 2)
+            #     g.add_edge(1, 3)
+            #     g.add_edge(2, 3)
+            #     g.add_edge(3, 4)
+            #     g.add_vertex(5)
+            #     g.add_edge(6, 7)
+            #     g.add_edge(6, 8)
+            #     self.assertEqual([{6, 7, 8}, {5}, {0, 1, 2, 3, 4}], strongly_connected_components(g))
+            #
+            #     # cp3 4.3 in https://visualgo.net/en/dfsbfs
+            #     g = Graph()
+            #     g.add_edge(0, 1)
+            #     g.add_edge(0, 4)
+            #     g.add_edge(1, 2)
+            #     g.add_edge(1, 5)
+            #     g.add_edge(2, 3)
+            #     g.add_edge(2, 6)
+            #     g.add_edge(3, 7)
+            #     g.add_edge(4, 8)
+            #     g.add_edge(5, 6)
+            #     g.add_edge(5, 10)
+            #     g.add_edge(6, 11)
+            #     g.add_edge(7, 12)
+            #     g.add_edge(8, 9)
+            #     g.add_edge(9, 10)
+            #     g.add_edge(10, 11)
+            #     g.add_edge(11, 12)
+            #     # print(strongly_connected_components(g))
+            #     self.assertEqual([{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}], strongly_connected_components(g))
+            #
+            #     # cp3 4.4 dag in https://visualgo.net/en/dfsbfs
+            #     dg = Digraph()
+            #     dg.add_edge(0, 1)
+            #     dg.add_edge(0, 2)
+            #     dg.add_edge(1, 2)
+            #     dg.add_edge(1, 3)
+            #     dg.add_edge(2, 3)
+            #     dg.add_edge(2, 5)
+            #     dg.add_edge(3, 4)
+            #     dg.add_edge(7, 6)
+            #     # print(strongly_connected_components(dg))
+            #     self.assertEqual([{7}, {6}, {0}, {1}, {2}, {5}, {3}, {4}], strongly_connected_components(dg))
+            #
+            #     # cp3 4.9 in https://visualgo.net/en/dfsbfs
+            #     dg = Digraph()
+            #     dg.add_edge(0, 1)
+            #     dg.add_edge(1, 3)
+            #     dg.add_edge(2, 1)
+            #     dg.add_edge(3, 2)
+            #     dg.add_edge(3, 4)
+            #     dg.add_edge(4, 5)
+            #     dg.add_edge(5, 7)
+            #     dg.add_edge(6, 4)
+            #     dg.add_edge(7, 6)
+            #     # print(strongly_connected_components(dg))
+            #     self.assertEqual([{0}, {1, 2, 3}, {4, 5, 6, 7}], strongly_connected_components(dg))
+            #
+            #     # cp3 4.17 dag in https://visualgo.net/en/dfsbfs
+            #     dg = Digraph()
+            #     dg.add_edge(0, 1)
+            #     dg.add_edge(0, 2)
+            #     dg.add_edge(0, 3)
+            #     dg.add_edge(1, 3)
+            #     dg.add_edge(1, 4)
+            #     dg.add_edge(2, 4)
+            #     dg.add_edge(3, 4)
+            #     # print(strongly_connected_components(dg))
+            #     self.assertEqual([{0}, {2}, {1}, {3}, {4}], strongly_connected_components(dg))
+            #
+            #     # cp3 4.18 dag bipartite in https://visualgo.net/en/dfsbfs
+            #     dg = Digraph()
+            #     dg.add_edge(0, 1)
+            #     dg.add_edge(0, 2)
+            #     dg.add_edge(1, 3)
+            #     dg.add_edge(2, 3)
+            #     dg.add_edge(3, 4)
+            #     print(strongly_connected_components(dg))
+            #     self.assertEqual([{0}, {2}, {1}, {3}, {4}], strongly_connected_components(dg))
+            #
+            #     # cp3 4.19 bipartite in https://visualgo.net/en/dfsbfs
+            #     dg = Digraph()
+            #     dg.add_edge(0, 1)
+            #     dg.add_edge(0, 4)
+            #     dg.add_edge(1, 2)
+            #     dg.add_edge(2, 1)
+            #     dg.add_edge(2, 3)
+            #     print(strongly_connected_components(dg))
+            #     self.assertEqual([{0}, {4}, {1, 2}, {3}], strongly_connected_components(dg))
