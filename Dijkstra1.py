4fb31f3cfb248b13cae54d94c21ad5891748837b
vpc20 <cuevasvp@yahoo.com>
2024-12-17 14:51:22 +0800

    new

---
+++ Dijkstra1.py
@@ -0,0 +1,96 @@
+import sys
+from heapq import heappop, heappush
+
+
+def dijkstra(g, src, tgt):
+    dist = {}  # distances
+    pred = {}  # predecessors
+    for u in g.keys():
+        dist[u] = sys.maxsize
+        pred[u] = None
+    dist[src] = 0
+    minq = [(0, src)]
+
+    while minq:
+        d, u = heappop(minq)
+        for v in g[u]:
+            d = dist[u] + w[(u, v)]
+            if d < dist[v]:  # relaxation
+                dist[v] = d
+                pred[v] = u
+                heappush(minq, (d, v))
+
+    u = tgt
+    path = [u]
+    while u != src:
+        u = pred[u]
+        path.append(u)
+    path.reverse()
+
+    return dist[tgt], path
+
+
+# def dijkstra(g, src):
+#     dist = {}  # distances
+#     pred = {}  # predecessors
+#     for u in g.vertices:
+#         dist[u] = sys.maxsize
+#         pred[u] = None
+#     dist[src] = 0
+#
+#     minq = [(0, src)]
+#     while minq:
+#         d, u = heappop(minq)
+#         if dist[u] < d:  # skip if the value in dist is smaller
+#             continue
+#         for v in g.neighbors(u):
+#             d = dist[u] + g.weight(u, v)
+#             if d < dist[v]:  # relaxation
+#                 dist[v] = d
+#                 pred[v] = u
+#                 heappush(minq, (dist[v], v))  # could have duplicate vertices in minq
+#     return pred, dist
+
+
+# to get the path, refer to the predecessors dictionary
+
+
+if __name__ == '__main__':
+    # dg = Digraph()  # clrs example
+    # dg.add_weighted_edges_from([('s', 't', 10), ('s', 'y', 5), ('t', 'x', 1), ('t', 'y', 2), ('x', 'z', 4),
+    #                             ('y', 't', 3), ('y', 'x', 9), ('y', 'z', 2), ('z', 's', 7), ('z', 'x', 6)])
+    # print(dg)
+    # print(dijkstra(dg, 's'))
+
+    # print('-----')
+    # dg = Digraph()  # graph theory example youtube
+    # dg.add_weighted_edges_from([(0, 1, 4), (0, 2, 1), (1, 3, 1), (2, 1, 2), (2, 3, 5), (3, 4, 3)])
+    # print(dg)
+    # print(dijkstra(dg, 0))
+    #
+    # print('-----')
+    # # cp3 4.17 in https://visualgo.net/en/sssp
+    # dg = Digraph()
+    # dg.add_weighted_edges_from([(0, 1, 2), (0, 2, 6), (0, 3, 7), (1, 3, 3), (1, 4, 6),
+    #                             (2, 4, 1), (3, 4, 5)])
+    # print(dg)
+    # print(dijkstra(dg, 0))
+
+    g = {'s': ['t', 'y'], 't': ['x', 'y'], 'y': ['t', 'x', 'z'], 'x': ['z'], 'z': ['s', 'x']}
+    # for k in g.keys():
+    #     for v in g[k]:
+    #         print(f'({k}, {v})')
+    w = {('s', 't'): 10,
+         ('s', 'y'): 5,
+         ('t', 'x'): 1,
+         ('t', 'y'): 2,
+         ('y', 't'): 3,
+         ('y', 'x'): 9,
+         ('y', 'z'): 2,
+         ('x', 'z'): 4,
+         ('z', 's'): 7,
+         ('z', 'x'): 6}
+
+    dist, path = dijkstra(g, 's', 'x')
+    print(dist)
+    print(path)
